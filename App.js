a78d0413af1c194794172dabfafae33e891b4995
Dazzler123 <dasinduhewagamage@gmail.com>
2023-05-10 21:36:23 +0530

    Content updated

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Alert, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
@@ -7,6 +7,12 @@ export default function App() {
       <Text>This is my first ever React Native app!</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+//        onPress={onPressLearnMore}
+        title="Learn More"
+        color="black"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
