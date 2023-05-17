7f4a340f400fa8ba4a962df7775a54176f7514a7
Dazzler123 <dasinduhewagamage@gmail.com>
2023-05-17 10:52:34 +0530

    Content updated

--- App.js
+++ App.js
@@ -1,27 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Alert, Text, View, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
-export default function App() {
+const SpotifyApp = () => {
   return (
     <View style={styles.container}>
-      <Text>This is my first ever React Native app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button
-//        onPress={onPressLearnMore}
-        title="Learn More"
-        color="black"
-        accessibilityLabel="Learn more about this purple button"
-      />
+      <View style={styles.header}>
+        <Text style={styles.title}>Spotify</Text>
+      </View>
+      <View style={styles.content}>
+        <Image
+          source={require('./background.jpg')}
+          style={styles.logo}
+        />
+        <Text style={styles.description}>
+          Listen to your favorite music anytime, anywhere.
+        </Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Open Spotify</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  header: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    padding: 16,
+    backgroundColor: '#1DB954',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  content: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 24,
+  },
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginBottom: 24,
+  },
+  button: {
+    backgroundColor: '#1DB954',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 24,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
+
+export default SpotifyApp;
